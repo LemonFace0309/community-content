507299e3158a01e5f36a208e3b4228797c0c5fa2
ryan <ryanj.belke@gmail.com>
2020-04-15 20:09:41 -0500

    step 2 complete

--- tutorials/code/next-strapi-hooks-deliveroo/frontend/pages/index.js
+++ tutorials/code/next-strapi-hooks-deliveroo/frontend/pages/index.js
@@ -1,15 +1,39 @@
 /* /pages/index.js */
-import { Button, Alert } from "reactstrap";
-import Layout from "../components/Layout";
-export default () => {
+import React, { useState } from "react";
+
+import { Col, Input, InputGroup, InputGroupAddon, Row } from "reactstrap";
+import RestaurantList from "../components/RestaurantList";
+import withData from "../lib/apollo";
+
+function Index() {
+  const [query, updateQuery] = useState("");
   return (
-    <Layout>
-      <div>
-        <Alert color="primary">
-          Hello Project is strapi-next with Bootstrap
-        </Alert>
-        &nbsp; <Button color="primary">Hello from nextjs</Button>
-      </div>
-    </Layout>
+    <div className="container-fluid">
+      <Row>
+        <Col>
+          <div className="search">
+            <InputGroup>
+              <InputGroupAddon addonType="append"> Search </InputGroupAddon>
+              <Input
+                onChange={(e) =>
+                  updateQuery(e.target.value.toLocaleLowerCase())
+                }
+                value={query}
+              />
+            </InputGroup>
+          </div>
+          <RestaurantList search={query} />
+        </Col>
+      </Row>
+      <style jsx>
+        {`
+          .search {
+            margin: 20px;
+            width: 500px;
+          }
+        `}
+      </style>
+    </div>
   );
-};
+}
+export default withData(Index);
